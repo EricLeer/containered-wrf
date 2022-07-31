d521b688e05d88c9f55fa3f27bcbb60388074c6a
Eric Leerssen <eric.leerssen@gmail.com>
2022-07-31 12:00:23 +0000

    Add loading and decompressing of geog files

---
+++ src/load_wrf_geog.py
@@ -0,0 +1,25 @@
+import requests
+import logging
+import tarfile
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+WRF_GEOG_URL = "https://www2.mmm.ucar.edu/wrf/src/wps_files/geog_low_res_mandatory.tar.gz"
+GEOG_FILE_PATH = "geog_low_res_mandatory.tar.gz"
+def get_geog_data():
+    logging.info("Loading geog data")
+    with open(GEOG_FILE_PATH, 'wb') as file:
+        r = requests.get(WRF_GEOG_URL)
+        file.write(r.content)
+
+def decompres_geog_files():
+    logging.info("Decompressing geog tar file")
+    with tarfile.open(GEOG_FILE_PATH) as file:
+        file.extractall()
+    logging.info("File decompresed, removing tar file")
+    os.remove(GEOG_FILE_PATH)
+
+if __name__ == "__main__":
+    get_geog_data()
+    decompres_geog_files()
