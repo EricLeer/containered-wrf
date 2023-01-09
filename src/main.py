7ca0e2aee2af0efb980bf005c57bcdc975271fd1
Eric Leerssen <eric.leerssen@gmail.com>
2023-01-09 10:06:31 +0100

    Add adding of local gfs files

--- src/main.py
+++ src/main.py
@@ -46,10 +46,10 @@ def write_namelist_yaml(namelist_yaml):
         return tmp.name
 
 
-def load_gfs_files(forecast_start_date):
+def load_gfs_files_remote(forecast_start_date):
     gfs_url = "ftpprd.ncep.noaa.gov"
     gfs_path = f"pub/data/nccf/com/gfs/prod/gfs.{forecast_start_date.strftime('%Y%m%d')}/00/atmos/"
-
+    
     ftp = ftplib.FTP(gfs_url)
     ftp.login()
     # ftp.login("UserName", "Password")
@@ -63,6 +63,14 @@ def load_gfs_files(forecast_start_date):
             ftp.retrbinary("RETR " + gfs_filename, f.write)
     ftp.quit()
 
+def load_gfs_files_local(forecast_start_date, local_file_path, datetime="2022080100"):
+    for i in range(0, 25, 3):
+        gfs_filename = f"{local_file_path}/gfs.0p25.{datetime}.f{i:03d}.grib2"
+        local_filename = f"data/GFS_{i:02d}"
+
+        logging.info(f"Loading local file {gfs_filename} to {local_filename}")
+
+        shutil.copy(gfs_filename, local_filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create wrf namelist files")
@@ -78,13 +86,18 @@ if __name__ == "__main__":
     create_namelist(forecast_start_date.strftime("%Y-%m-%d_%H:%M:%S"))
 
     logging.info("Loading GFS initialization files..")
-    load_gfs_files(forecast_start_date)
+    
+    gfs_local_path = "/home/wrf/external_data/gfs_forecasts"
+    if os.path.exists(gfs_local_path):
+        load_gfs_files_local(forecast_start_date, gfs_local_path, datetime=forecast_start_date.strftime("%Y%m%d00"))
+    else:
+        load_gfs_files_remote(forecast_start_date)
 
     # Check for presence of geog file, if it is not there load it, else move it to the local location
-    geog_path = "/home/wrf/data/WPS_GEOG_LOW_RES"
+    geog_path = "/home/wrf/external_data/WPS_GEOG_LOW_RES"
     if os.path.exists(geog_path):
         logging.info(f"GEOG data already loaded at {geog_path}, just moving files.")
-        shutil.move(geog_path, "/home/wrf/")
+        shutil.copytree(geog_path, "/home/wrf/WPS_GEOG_LOW_RES")
     else:
         get_geog_data()
         decompres_geog_files()
